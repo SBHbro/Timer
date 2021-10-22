b0d33e849468c317492708358bbb957233bb956b
SBHbro <ssd018@gmail.com>
2021-10-22 15:32:43 +0900

    Update Time Interval

---
+++ Timer/m_comboBox.cpp
@@ -0,0 +1,34 @@
+﻿// m_comboBox.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Timer.h"
+#include "m_comboBox.h"
+#include "afxdialogex.h"
+
+
+// m_comboBox 대화 상자
+
+IMPLEMENT_DYNAMIC(m_comboBox, CDialogEx)
+
+m_comboBox::m_comboBox(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_TIMER_DIALOG, pParent)
+{
+
+}
+
+m_comboBox::~m_comboBox()
+{
+}
+
+void m_comboBox::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(m_comboBox, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// m_comboBox 메시지 처리기
---
+++ Timer/m_comboBox.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// m_comboBox 대화 상자
+
+class m_comboBox : public CDialogEx
+{
+	DECLARE_DYNAMIC(m_comboBox)
+
+public:
+	m_comboBox(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~m_comboBox();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TIMER_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
